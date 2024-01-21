58d88672bd15271fc04570ba4ded4ab9815b158c
Adam Alrasi <adam.alrasi1@gmail.com>
2024-01-21 12:32:49 +0000

    219. Contains Duplicate II

---
+++ Easy/219. Contains Duplicate II.js
@@ -0,0 +1,33 @@
+/*
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1
+Output: true
+Example 3:
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+*/
+
+var containsNearbyDuplicate = function(nums, k) {
+    const numsMap = {};
+    for(let i = 0; i < nums.length; i++){
+        if(numsMap[nums[i]] !== undefined
+            && i - numsMap[nums[i]] <= k){
+                return true
+            }
+        numsMap[nums[i]] = i;
+    }
+    return false
+};
+
+// Time complexity: O(n)
+// Space complexity: O(n)
